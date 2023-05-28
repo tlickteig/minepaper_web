525f456372748c9c29632bc1d95996803d66d207
root <root@pop-os.localdomain>
2023-05-28 12:10:02 -0500

    Moved all site assets into src directory

---
+++ src/partial/head.php
@@ -0,0 +1,6 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="./lib/jquery/jquery-3.7.0.min.js"></script>
+    <script src="./lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+</head>
