c3f328f425d706845e2d2e9eb16d18fb2e07692d
Timothy Lickteig <timothylickteig@Timothys-Mac-mini.local>
2023-05-30 21:07:35 -0500

    Nova's work -fix navigation to home -point github links toward latest release -basic html for tabs on downloads page, Javascript not included yet

--- src/downloads.php
+++ src/downloads.php
@@ -3,7 +3,29 @@
     <body id="bdyMainWindow">
         <?php require "partial/header.php"; ?>
         <div id="dvMainWindow" class="pixel-corners-radius-10-px-border-1px">
+            <div id="downloadWelcome">
 
+            </div>
+            <ul class="nav nav-tabs" id="releaseTypeTab" role="tablist">
+                <li class="nav-item" role="presentation" id="windows-download-tab">
+                    <a class="nav-link" href="#" role="tab" data-bs-target="#windows-download-tab" data-bs-toggle="tab">Windows</a>
+                </li>
+                <li class="nav-item" role="presentation" id="mac-download-tab">
+                    <a class="nav-link" href="#" role="tab" data-bs-target="#mac-download-tab" data-bs-toggle="tab">MacOS</a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" role="tabpanel" aria-labelledby="windows-download-tab">
+                    <div class="btn">
+                        <a id="windowsDownload" href="https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip" role="button">For Windows</a>
+                    </div>
+                </div>
+                <div class="tab-pane" role="tabpanel" aria-labelledby="mac-download-tab">
+                    <div class="btn">
+                        <a id="macDownload" href="https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip" role="button">For MacOS</a>
+                    </div>
+                </div>
+            </div>
         </div>
     <body>
 </html>
--- src/partial/header.php
+++ src/partial/header.php
@@ -1,7 +1,7 @@
 <header>
     <div id="dvHeader">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <nav class="navbar-brand" href="#">MinePaper.net</nav>
+            <a class="navbar-brand" href="index.php">MinePaper.net</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#siteNav" aria-controls="siteNav" aria-expanded="false" aria-label="Toggle site navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -9,7 +9,7 @@
             <div class="collapse navbar-collapse" id="siteNav">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="downloads.php">Download</a>
@@ -20,11 +20,11 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="sourceOptions" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="Toggle Github source navigation">Github</a> 
                         <div class="dropdown-menu"  aria-labelledby="sourceOptions">
-                            <a class="dropdown-item" href="https://github.com/tlickteig/minepaper_uwp">Windows</a>
+                            <a class="dropdown-item" href="https://github.com/tlickteig/minepaper_uwp/releases/latest">Windows</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="https://github.com/tlickteig/minepaper_mac">Mac</a>
+                            <a class="dropdown-item" href="https://github.com/tlickteig/minepaper_mac/releases/latest">Mac</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="https://github.com/tlickteig/minepaper_web">Web</a>
+                            <a class="dropdown-item" href="https://github.com/tlickteig/minepaper_web/releases/latest">Web</a>
                         </div>
                     </li>
                 </ul>
