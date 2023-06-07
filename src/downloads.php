9c99ccd8b2e49a9504206e2c4565aa462744bf38
Timothy Lickteig <timmattmelnat@gmail.com>
2023-06-06 20:26:46 -0500

    make tabs work -Nova

--- src/downloads.php
+++ src/downloads.php
@@ -7,23 +7,19 @@
 
             </div>
             <ul class="nav nav-tabs" id="releaseTypeTab" role="tablist">
-                <li class="nav-item" role="presentation" id="windows-download-tab">
-                    <a class="nav-link" href="#" role="tab" data-bs-target="#windows-download-tab" data-bs-toggle="tab">Windows</a>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="windows-download-tab" role="tab" data-bs-target="#download-option-windows" data-bs-toggle="tab">Windows</button>
                 </li>
-                <li class="nav-item" role="presentation" id="mac-download-tab">
-                    <a class="nav-link" href="#" role="tab" data-bs-target="#mac-download-tab" data-bs-toggle="tab">MacOS</a>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="mac-download-tab" role="tab" data-bs-target="#download-option-mac" data-bs-toggle="tab">MacOS</button>
                 </li>
             </ul>
             <div class="tab-content">
-                <div class="tab-pane active" role="tabpanel" aria-labelledby="windows-download-tab">
-                    <div class="btn">
-                        <a id="windowsDownload" href="https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip" role="button">For Windows</a>
-                    </div>
+                <div class="tab-pane fade show active" id="download-option-windows" role="tabpanel" aria-labelledby="windows-download-tab">
+                        <a class="btn" id="windowsDownload" href="https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip" role="button">For Windows</a>
                 </div>
-                <div class="tab-pane" role="tabpanel" aria-labelledby="mac-download-tab">
-                    <div class="btn">
-                        <a id="macDownload" href="https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip" role="button">For MacOS</a>
-                    </div>
+                <div class="tab-pane fade" id="download-option-mac" role="tabpanel" aria-labelledby="mac-download-tab">
+                        <a class="btn" id="macDownload" href="https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip" role="button">For MacOS</a>
                 </div>
             </div>
         </div>
