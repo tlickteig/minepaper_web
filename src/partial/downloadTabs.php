6120583a5bacbd4446207ac79275b67847f2fe56
Timothy Lickteig <timmattmelnat@gmail.com>
2023-06-10 17:00:43 -0500

    more readability cleanup -Nova

--- src/partial/downloadTabs.php
+++ src/partial/downloadTabs.php
@@ -1,36 +1,30 @@
 <?php 
     $tabInfo =array(
-        array("windows" , "https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip", "Windows", true),
-        array("mac", "https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip", "MacOS", false)
+        array("quickName" => "windows" , "link" => "https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip", "fancyName" => "Windows", "isDefaultTab" => true),
+        array("quickName" => "mac", "link" =>  "https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip",  "fancyName" => "MacOS",  "isDefaultTab" => false)
     );
 ?>
 
 <ul class="nav nav-tabs" id="releaseTypeTab" role="tablist">
 <?php 
     foreach ($tabInfo as $osType):
-        $osQuickName = $osType[0];
-        $osFancyName = $osType[2];
-        $downloadLink = $osType[1];
-        $isActiveClass = $osType[3] ? "active" : ""; 
+        $isActiveClass = $osType["isDefaultTab"] ? "active" : ""; 
 ?>
     <li class="nav-item" role="presentation">
-        <button role="tab" data-bs-toggle="tab" class='nav-link <?= $isActiveClass ?>' data-bs-target='#download-option-<?= $osQuickName ?>' id='<?=$osQuickName?>-download-tab' id='<?=$osQuickName?>-download-tab'>
-            <?=$osFancyName?>
+        <button role="tab" data-bs-toggle="tab" class='nav-link <?= $isActiveClass ?>' data-bs-target='#download-option-<?= $osType["quickName"] ?>' id='<?= $osType["quickName"] ?>-download-tab'>
+            <?= $osType["fancyName"] ?>
         </button>
     </li>
 <?php endforeach; ?>
 </ul>
 <div class="tab-content">
 <?php foreach ($tabInfo as $osType):
-    $osQuickName = $osType[0];
-    $osFancyName = $osType[2];
-    $downloadLink = $osType[1];
-    $isActiveClass = $osType[3] ? "show active" : ""; 
+        $isActiveClass = $osType["isDefaultTab"] ? "active show" : ""; 
 ?>
-    <div role='tabpanel' class='tab-pane fade <?= $isActiveClass ?>' id='download-option-<?= $osQuickName ?>' aria-labelledby='<?= $osQuickName ?>-download-tab'>
+    <div role='tabpanel' class='tab-pane fade <?= $isActiveClass ?>' id='download-option-<?= $osType["quickName"] ?>' aria-labelledby='<?= $osType["quickName"] ?>-download-tab'>
         <div class="d-grid gap-2 col-8 col-lg-6 mx-auto">
-            <a class="btn btn-outline-dark btn-lg" role="button" id='<?= $osQuickName ?>-download-link' href='<?= $downloadLink ?>' >
-                Download MinePaper for <?= $osFancyName ?>
+            <a class="btn btn-outline-dark btn-lg" role="button" id='<?= $osType["quickName"] ?>-download-link' href='<?= $osType["link"] ?>' >
+                Download MinePaper for <?= $osType["fancyName"] ?>
             </a>
         </div>
     </div>
