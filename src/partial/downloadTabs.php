816073fac4d45bf7163ba9fd53d91092c537dd88
Timothy Lickteig <timmattmelnat@gmail.com>
2023-06-10 16:12:28 -0500

    the promised cleanup the variable mess -Nova

--- src/partial/downloadTabs.php
+++ src/partial/downloadTabs.php
@@ -8,33 +8,29 @@
 <ul class="nav nav-tabs" id="releaseTypeTab" role="tablist">
 <?php 
     foreach ($tabInfo as $osType):
-        $isActive = $osType[3] ? "active" : ""; 
-        $buttonClasses = "class='nav-link  $isActive'";
-        $buttonTarget = "data-bs-target='#download-option-$osType[0]'";
-        $buttonLabelledBy = "aria-labelledby='$osType[0]-download-tab'";
-        $buttonId = "id='$osType[0]-download-tab'";
-        $buttonLabelledBy = "aria-labelledby='download-option-$osType[0]'";
+        $osQuickName = $osType[0];
         $osFancyName = $osType[2];
+        $downloadLink = $osType[1];
+        $isActiveClass = $osType[3] ? "active" : ""; 
 ?>
     <li class="nav-item" role="presentation">
-        <button role="tab" data-bs-toggle="tab" <?php echo "$buttonClasses $buttonId $buttonTarget"; ?>><?php echo $osFancyName?></button>
+        <button role="tab" data-bs-toggle="tab" class='nav-link <?= $isActiveClass ?>' data-bs-target='#download-option-<?= $osQuickName ?>' id='<?=$osQuickName?>-download-tab' id='<?=$osQuickName?>-download-tab'>
+            <?=$osFancyName?>
+        </button>
     </li>
 <?php endforeach; ?>
 </ul>
 <div class="tab-content">
-<?php foreach ($tabInfo as $osType): 
-    $isActive = $osType[3] ? "show active" : ""; 
-    $tabClasses = "class='tab-pane fade $isActive'";
-    $tabId = "id='download-option-$osType[0]'";
-    $tabLabelledBy = "aria-labelledby='$osType[0]-download-tab'";
-    $linkId = "id='$osType[0]-download-link'";
-    $link = "href='$osType[1]'";
+<?php foreach ($tabInfo as $osType):
+    $osQuickName = $osType[0];
     $osFancyName = $osType[2];
+    $downloadLink = $osType[1];
+    $isActiveClass = $osType[3] ? "show active" : ""; 
 ?>
-    <div role='tabpanel' <?php echo "$tabClasses $tabId $tabLabelledBy"; ?>>
+    <div role='tabpanel' class='tab-pane fade <?= $isActiveClass ?>' id='download-option-<?= $osQuickName ?>' aria-labelledby='<?= $osQuickName ?>-download-tab'>
         <div class="d-grid gap-2 col-8 col-lg-6 mx-auto">
-            <a class="btn btn-outline-dark btn-lg" role="button" <?php echo "$linkId $link";?> >
-                Download MinePaper for <?php echo $osFancyName; ?>
+            <a class="btn btn-outline-dark btn-lg" role="button" id='<?= $osQuickName ?>-download-link' href='<?= $downloadLink ?>' >
+                Download MinePaper for <?= $osFancyName ?>
             </a>
         </div>
     </div>
