c796b3a5ca4b2bc911ac62cdae1bd5b386e4dc8b
Timothy Lickteig <timmattmelnat@gmail.com>
2023-06-15 19:31:10 -0500

    improve constants readability and intent improve comments  -Nova

--- src/partial/downloadTabs.php
+++ src/partial/downloadTabs.php
@@ -1,22 +1,17 @@
 <?php 
     require_once "util/utilities.php";
-    $tabInfo = array(
-        Constants::$windowsArr, 
-        Constants::$macArr    
-    );
-    // Check for a quickName matching the OS returned from the User Agent
-    $info = get_os_name("quickName");
-    $tabToClick = "$info-download-tab";
+    
+    // Get our active tabs
+    $tabInfo = Constants::get_tab_info();
+
+    // Check for a quickName matching the OS returned from the User Agent. Use default if not found
+    $activeOS = get_os_name("quickName") ?? Constants::$defaultTabName;
 ?>
 
 <ul class="nav nav-tabs" id="releaseTypeTab" role="tablist">
 <?php 
     foreach ($tabInfo as $osType):
-        if ($tabToClick != "") {
-            $isActiveClass = $osType["quickName"] == $info ? "active" : "";
-        } else {
-            $isActiveClass = $osType["isDefaultTab"] ? "active" : ""; 
-        }
+        $isActiveClass = $osType["quickName"] == $activeOS ? "active" : "";
 ?>
     <li class="nav-item" role="presentation">
         <button role="tab" data-bs-toggle="tab" class='nav-link <?= $isActiveClass ?>' data-bs-target='#download-option-<?= $osType["quickName"] ?>' id='<?= $osType["quickName"] ?>-download-tab'>
@@ -26,17 +21,14 @@
 <?php endforeach; ?>
 </ul>
 <div class="tab-content">
-<?php foreach ($tabInfo as $osType):
-        if ($tabToClick != "") {
-            $isActiveClass = $osType["quickName"] == $info ? "active show" : "";
-        } else {
-            $isActiveClass = $osType["isDefaultTab"] ? "active show" : ""; 
-        }
+<?php 
+    foreach ($tabInfo as $osType):
+        $isActiveClass = $osType["quickName"] == $activeOS ? "active show" : "";
 ?>
     <div role='tabpanel' class='tab-pane fade <?= $isActiveClass ?>' id='download-option-<?= $osType["quickName"] ?>' aria-labelledby='<?= $osType["quickName"] ?>-download-tab'>
         <div class="d-grid gap-2 col-8 col-lg-6 mx-auto">
-            <a class="btn btn-outline-dark btn-lg" role="button" id='<?= $osType["quickName"] ?>-download-link' href='<?= $osType["link"] ?>' >
-                Download MinePaper for <?= $osType["fancyName"] ?>
+            <a class="btn btn-outline-dark btn-lg" role="button" id='<?= $osType["quickName"] ?>-download-link' href='<?= $osType["releaseLink"] ?>' >
+                Download <?= Constants::$projectName ?> for <?= $osType["fancyName"] ?>
             </a>
         </div>
     </div>
--- src/util/constants.php
+++ src/util/constants.php
@@ -1,8 +1,52 @@
 <?php
 class Constants {
-    public static $siteName = "MinePaper.Net";
+    public static $projectName = "MinePaper";
+    // The default active download tab if user agent can not be found. Match with a "quickName"
+    public static $defaultTabName = "windows";
     
-    public static $windowsArr = array("name"=>"Windows", "regex" => "(Windows|Wince|Win)", "quickName" => "windows", "fancyName" => "Windows", "link" => "https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip", "isDefaultTab" => false);
-    public static $macArr = array("name"=>"Mac", "regex" => "(Mac)", "quickName" =>  "mac", "fancyName" => "MacOS", "link" => "https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip", "isDefaultTab" => true);
+    // Active tabs, in the order they will appear. 
+    public static function get_tab_info() { 
+        return array(
+            Constants::$macArr,
+            Constants::$windowsArr
+        );
+    }
+    
+    // Github items, in the order they will appear in the dropdown.
+    public static function get_github_info() { 
+        return array(
+            Constants::$macArr,
+            Constants::$webArr,
+            Constants::$windowsArr
+        );
+    }
+
+    // Defining details for the page. 
+    // Name, Regex, QuickName, FancyName, and ReleaseLink necessary for item to appear in downloads. 
+    // Name, QuickName, FancyName, GitHubLink necessary for item to appear in Github dropdown.
+    public static $windowsArr = array(
+        "name" => "Windows", 
+        "regex" => "(Windows|Wince|Win)", 
+        "quickName" => "windows", 
+        "fancyName" => "Windows", 
+        "releaseLink" => "https://www.github.com/tlickteig/minepaper_uwp/releases/latest/download/minepaper.zip", 
+        "githubLink" => "https://www.github.com/tlickteig/minepaper_uwp/releases/latest/"
+    );
+
+    public static $macArr = array(
+        "name" => "Mac", 
+        "regex" => "(Mac)", 
+        "quickName" => "mac", 
+        "fancyName" => "MacOS", 
+        "releaseLink" => "https://www.github.com/tlickteig/minepaper_mac/releases/latest/download/minepaper.zip", 
+        "githubLink" => "https://www.github.com/tlickteig/minepaper_mac/releases/latest/"
+    );
+
+    public static $webArr = array(
+        "name" => "Web", 
+        "quickName" => "web", 
+        "fancyName" => "Website", 
+        "githubLink" => "https://www.github.com/tlickteig/minepaper_web/releases/latest/",
+    );
 }
 ?>
