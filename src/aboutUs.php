5d62816cd071d89764011d01dbcbb8eb58f50195
Timothy Lickteig <timmattmelnat@gmail.com>
2023-06-27 12:51:47 -0500

    Added header to about us screen

--- src/aboutUs.php
+++ src/aboutUs.php
@@ -4,6 +4,7 @@
         <?php require "partial/header.php"; ?>
         <div id="dvMainWindow" class="pixel-corners-radius-10-px-border-1px">
             <div class="col-lg-8 mx-auto">
+                <h2>Privacy Policy</h2>
                 <?= Constants::$projectName ?> is dedicated to one thing, and one thing only: plastering your desktop with sweet ass Minecraft landscapes. It was the brain-child of two nerds who have way too much free time frankly, but it seems that the results are decent enough. Feel free to contribute to the project on Github, and dont forget to enjoy!
             </div>
         </div>
