6eacbb5e85a47f39acff3b3816528ed28258a12a
Nova Hickie <ambreejock@gmail.com>
2023-10-10 19:23:26 -0500

    improve spacing for blog index page

--- src/blog/index.php
+++ src/blog/index.php
@@ -1,8 +1,8 @@
-<?php 
-    $documentRoot = $_SERVER["DOCUMENT_ROOT"];
-    require_once("$documentRoot/util/blogutils.php");
+<?php
+$documentRoot = $_SERVER["DOCUMENT_ROOT"];
+require_once("$documentRoot/util/blogutils.php");
 
-    $articles = BlogUtils::fetch_article_list();
+$articles = BlogUtils::fetch_article_list();
 ?>
 
 <?php require "$documentRoot/common/pageTop.php" ?>
@@ -11,22 +11,24 @@
 </div>
 
 <div class="col-lg-10 mx-auto main-text">
-    <?php foreach($articles as $article) : ?>
-        <h4 style="text-align: center">
-            <a href="<?php echo $article->get_path(); ?>">
-                <?php echo $article->get_title(); ?>
-            </a>
-            - By <?php echo $article->get_author(); ?>
-        </h4>
-        <p style="text-align: center; color: gray">
-            <?php echo $article->get_description(); ?>
-        </p>
-        <?php if ($article->get_thumbnail_src() != "") : ?>
-            <div class="container">
-                <img class="col-md-6" style="width: 50%; object-fit: contain;" src="<?php echo $article->get_thumbnail_src(); ?>" />
-            </div>
-        <?php endif; ?>
-        <p></p>
+    <?php foreach ($articles as $article) : ?>
+        <div class="pb-5">
+            <h4 style="text-align: center">
+                <a href="<?php echo $article->get_path(); ?>">
+                    <?php echo $article->get_title(); ?>
+                </a>
+                - By <?php echo $article->get_author(); ?>
+            </h4>
+            <p style="text-align: center; color: gray">
+                <?php echo $article->get_description(); ?>
+            </p>
+            <?php if ($article->get_thumbnail_src() != "") : ?>
+                <div class="container">
+                    <img class="col-md-6" style="width: 50%; object-fit: contain;" src="<?php echo $article->get_thumbnail_src(); ?>" />
+                </div>
+            <?php endif; ?>
+            <p></p>
+        </div>
     <?php endforeach; ?>
 </div>
 
