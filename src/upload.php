723bae13d255e4ce3ee077790ff0506047b3b79f
Nova Hickie <ambreejock@gmail.com>
2023-10-14 09:07:58 -0500

    add guidelines for uploading images

--- src/upload.php
+++ src/upload.php
@@ -32,15 +32,15 @@ if (isset($_POST["submit"])) {
 
     if (empty($output_message)) {
         $target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
             $output_message = "Only .png and .jpeg files are allowed";
         }
     }
 
     if ($_FILES["uploadedFile"]["size"] > Constants::$fileUploadMaxSizeBytes) {
         $output_message = "Your file is too large. Please try a different one.";
-      }
+    }
 
     if (empty($output_message)) {
         $number_of_images_in_uploads = return_number_of_images_in_directory($target_dir);
@@ -62,7 +62,20 @@ if (isset($_POST["submit"])) {
 
 <?php require "$documentRoot/common/pageTop.php" ?>
 <h2>Upload an image to <?= Constants::$projectName ?>.net</h2>
-<form action="upload.php" method="post" enctype="multipart/form-data" class="pt-5">
+<div class="row justify-content-center">
+    <div class="col-6 py-4">
+        <div class="">Image Submission Guidelines</div>
+        <ul style="text-align:left;" class="py-4">
+            <li>Images should be taken by you.</li>
+            <li>Images should be taken at 1080p.</li>
+            <li>Images should have a 16x9 aspect ratio.</li>
+            <li>Images should not have text.</li>
+            <li>Texture packs and shaders are preferred but not required.</li>
+            <li>You agree to the use of your image by <?= Constants::$projectName ?>. </li>
+        </ul>
+    </div>
+</div>
+<form action="upload.php" method="post" enctype="multipart/form-data" class="pt-1">
     <input type="file" name="uploadedFile" class="pb-5">
     <br />
     <input type="submit" value="Upload Image" name="submit" class="py-1">
